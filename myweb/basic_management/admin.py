abc1191f3d41ae8895105899b2ee966a922db2fa
RandyKing <wzmf399444@gmail.com>
2017-11-22 20:46:44 +0800

    amend models in basic_management

--- myweb/basic_management/admin.py
+++ myweb/basic_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from basic_management.models import *
 # Register your models here.
+class Company_InfoAdmin(admin.ModelAdmin):
+	list_display=('comp_id',)
+
+admin.site.register(Company_Info,Company_InfoAdmin)
+admin.site.register(Employee_Info)
+admin.site.register(Client_Info)
--- myweb/basic_management/models.py
+++ myweb/basic_management/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Company_Info(models.Model):
     comp_id = models.UUIDField(primary_key=True)
     comp_name = models.CharField(max_length=100)
-    comp_address = models.EmialField(max_length=200)
+    comp_address = models.EmailField(max_length=200)
     comp_phonenumber = models.CharField(max_length=200)
     comp_EIN = models.CharField(max_length=50)
     comp_person_in_charge = models.CharField(max_length=20)
@@ -17,17 +17,17 @@ class Company_Info(models.Model):
 
 class Employee_Info(models.Model):
 	emp_id = models.UUIDField(primary_key=True)
-    emp_name = models.CharField(max_length=100)
-    emp_address = models.EmailField(max_length=200)
-    emp_phonenumber = models.CharField(max_length=200)
-    emp_gender = models.BooleanField()
-    emp_birthday = models.DateField()
-    comp_id=models.Foreignkey(Company_Info,models.DO_NOTHING)
+	emp_name = models.CharField(max_length=100)
+	emp_address = models.EmailField(max_length=200)
+	emp_phonenumber = models.CharField(max_length=200)
+	emp_gender = models.BooleanField()
+	emp_birthday = models.DateField()
+	comp_id=models.ForeignKey(Company_Info,models.DO_NOTHING)
 
 class Client_Info(models.Model):
 	client_id = models.UUIDField(primary_key=True)
-    client_name = models.CharField(max_length=100)
-    client_address = models.EmailField(max_length=200)
-    client_phonenumber = models.CharField(max_length=200)
-    client_gender = models.BooleanField()
-    client_birthday = models.DateField()
+	client_name = models.CharField(max_length=100)
+	client_address = models.EmailField(max_length=200)
+	client_phonenumber = models.CharField(max_length=200)
+	client_gender = models.BooleanField()
+	client_birthday = models.DateField()
