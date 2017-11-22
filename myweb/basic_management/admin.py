59d010f45c005161c6c58080800c5ab02eb9346d
RandyKing <wzmf399444@gmail.com>
2017-11-22 20:50:06 +0800

    add table to admin

--- myweb/basic_management/admin.py
+++ myweb/basic_management/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from basic_management.models import *
 # Register your models here.
-class Company_InfoAdmin(admin.ModelAdmin):
-	list_display=('comp_id',)
 
-admin.site.register(Company_Info,Company_InfoAdmin)
+
+admin.site.register(Company_Info)
 admin.site.register(Employee_Info)
 admin.site.register(Client_Info)
