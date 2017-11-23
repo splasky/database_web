1c5c00a60cc163d8b013b73b0e444aab766c3b5c
Splasky <henrychung860326@gmail.com>
2017-11-23 19:31:17 +0800

    Update migrates.

--- myweb/basic_management/admin.py
+++ myweb/basic_management/admin.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-11-23 17:23:14
+# Last modified: 2017-11-23 18:30:47
 
 from django.contrib import admin
 from basic_management.models import *
@@ -9,6 +9,6 @@ from basic_management.models import *
 admin.site.register(Company_Info)
 admin.site.register(Employee_Info)
 admin.site.register(Client_Info)
-admin.site.register(Categories)
+admin.site.register(Categorie)
 admin.site.register(Product_Information)
 admin.site.register(Manufacturer_Information)
--- myweb/basic_management/models.py
+++ myweb/basic_management/models.py
@@ -1,12 +1,11 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-11-23 16:45:22
+# Last modified: 2017-11-23 19:24:52
 from django.db import models
 
 
 class Company_Info(models.Model):
-    comp_id = models.UUIDField(primary_key=True)
     comp_name = models.CharField(max_length=100)
     comp_address = models.EmailField(max_length=200)
     comp_phonenumber = models.CharField(max_length=200)
@@ -16,9 +15,11 @@ class Company_Info(models.Model):
     comp_email = models.CharField(max_length=254)
     comp_introduction = models.TextField(blank=True, null=True)
 
+    class Mate:
+        db_table = 'company_info'
+
 
 class Employee_Info(models.Model):
-    emp_id = models.UUIDField(primary_key=True)
     emp_name = models.CharField(max_length=100)
     emp_address = models.EmailField(max_length=200)
     emp_phonenumber = models.CharField(max_length=200)
@@ -26,20 +27,25 @@ class Employee_Info(models.Model):
     emp_birthday = models.DateField()
     comp_id = models.ForeignKey(Company_Info, models.DO_NOTHING)
 
+    class Mate:
+        db_table = 'employee_info'
+
 
 class Client_Info(models.Model):
-    client_id = models.UUIDField(primary_key=True)
     client_name = models.CharField(max_length=100)
     client_address = models.EmailField(max_length=200)
     client_phonenumber = models.CharField(max_length=200)
     client_gender = models.BooleanField()
     client_birthday = models.DateField()
 
+    class Mate:
+        db_table = 'client_info'
 
-class Categories(models.Model):
+
+class Categorie(models.Model):
 
     """Merchandise Categories"""
-    Categories_name = models.CharField(max_length=100)
+    Categorie_name = models.CharField(max_length=100)
 
     class Mate:
         db_table = 'categories'
@@ -51,20 +57,17 @@ class Product_Information(models.Model):
     Product_name = models.CharField(max_length=100)
     Hight = models.FloatField()
     Weight = models.FloatField()
-    Categories_id = models.ForeignKey('Categories', on_delete=models.CASCADE)
+    Categories_id = models.ForeignKey(Categorie, on_delete=models.CASCADE)
 
     class Mate:
         db_table = 'product_information'
 
 
-class Manufacturer_Information(models.Model):
+class Manufacturer_Information(Company_Info):
 
     """Manufacturer's information"""
     Uniform_numbers = models.CharField(max_length=100)
-    ManuFacturer_name = models.CharField(max_length=100)
     Total_capital = models.IntegerField()
-    Name_of_representative = models.CharField(max_length=100)
-    Company_location = models.CharField(max_length=200)
 
     class Mate:
         db_table = 'manufacturer information'
