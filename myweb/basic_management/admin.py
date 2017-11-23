29cdf110cd47ec8a9940ef90af3b254b0e8aa41c
Splasky <henrychung860326@gmail.com>
2017-11-23 17:25:41 +0800

    Register model.

--- myweb/basic_management/admin.py
+++ myweb/basic_management/admin.py
@@ -1,8 +1,14 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# Last modified: 2017-11-23 17:23:14
+
 from django.contrib import admin
 from basic_management.models import *
-# Register your models here.
-
 
 admin.site.register(Company_Info)
 admin.site.register(Employee_Info)
 admin.site.register(Client_Info)
+admin.site.register(Categories)
+admin.site.register(Product_Information)
+admin.site.register(Manufacturer_Information)
