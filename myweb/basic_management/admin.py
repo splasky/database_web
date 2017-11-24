417b69dbe38b2c903e4cf3ed15fc948fdd50fb66
RandyKing <wzmf399444@gmail.com>
2017-11-24 17:46:54 +0800

    complete models and admin

--- myweb/basic_management/admin.py
+++ myweb/basic_management/admin.py
@@ -14,15 +14,30 @@ class Employee_InfoAdmin(admin.ModelAdmin):
     list_display = ('name', 'gender', 'comp_id')
     list_filter = ('gender',)
     search_fields = ('name', 'phonenumber')
-# class Client_InfoAdmin(admin.ModelAdmin):
 
-    # class CategoriesAdmin(admin.ModelAdmin):
 
-    # class Product_InformationAdmin(admin.ModelAdmin):
+class Client_InfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phonenumber', 'gender')
 
-    # class Manufacturer_InformationAdmin(admin.ModelAdmin):
+
+class CategoriesAdmin(admin.ModelAdmin):
+
+    list_display = ('Categories_name',)
+
+
+class Product_InformationAdmin(admin.ModelAdmin):
+
+    list_display = ('Product_name', 'Hight', 'Weight', 'Categories_id')
+
+
+class Manufacturer_InformationAdmin(admin.ModelAdmin):
+    list_display = ('Uniform_numbers', 'ManuFacturer_name',
+                    'Name_of_representative')
 
 
 admin.site.register(Company_Info, Company_InfoAdmin)
 admin.site.register(Employee_Info, Employee_InfoAdmin)
-# admin.site.register(Client_Info, Client_InfoAdmin)
+admin.site.register(Client_Info, Client_InfoAdmin)
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Product_Information, Product_InformationAdmin)
+admin.site.register(Manufacturer_Information, Manufacturer_InformationAdmin)
